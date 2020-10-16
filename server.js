6219aa4d9d2a15258a3b8ed6279ca508f03e8054
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2020-10-16 16:10:16 +0900

    mend

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.get("*/client.js", function (request, response) {
+  response.sendFile(__dirname + '/public/client.js');
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("*", function (request, response) {
+app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
